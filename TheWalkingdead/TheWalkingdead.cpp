724cd6aa75c0ce76232a78a987bfd329545ae694
aleixportapapell <aleixportapapell@S1-43>
2017-02-23 11:20:20 +0100

    PRACTICA 3 THE WALKING DEAD

---
+++ TheWalkingdead/TheWalkingdead.cpp
@@ -0,0 +1,39 @@
+﻿// TheWalkingdead.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>​using namespace std;#include <string>​ #include <ctime>​
+
+enum Weapon { FISTS, GUN, SHOTGUN, REVOLVER, SNIPER, MACHINE_GUN, MAX };
+
+class Player {
+	Weapon weapon;
+	float precision;
+	int life;
+	
+};
+
+class Zombie {
+	int distanceToPlayer;
+	float speed;
+	float damage;
+	int life;
+	
+};
+
+/*void attack(Zombie &); {
+
+}
+
+void attack(Player &); {
+
+}*/
+
+
+
+
+int main()
+{
+    return 0;
+}
+
